cfd29cd587a95946e4753a354552b01341c359ed
Carlos Bronze <170220+bronze@users.noreply.github.com>
2025-08-03 21:36:15 -0300

    trying a right prompt

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,4 @@
 # oh-my-posh init fish | source
-oh-my-posh init fish --config ~/.poshthemes/nord.recolor.omp.toml | source
+oh-my-posh init fish --config ~/.poshthemes/tonybaloney.nord.omp.json | source
 set -gx PATH /home/linuxbrew/.linuxbrew/bin $PATH
 set fish_greeting
