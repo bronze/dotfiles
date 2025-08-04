65f2bf6733c47d19ea14facc705259c77c7e35f6
Carlos Bronze <170220+bronze@users.noreply.github.com>
2025-08-03 21:17:02 -0300

    updates

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,4 @@
 # oh-my-posh init fish | source
-oh-my-posh init fish --config ~/.poshthemes/nord.recolor.omp.json | source
+oh-my-posh init fish --config ~/.poshthemes/nord.recolor.omp.toml | source
 set -gx PATH /home/linuxbrew/.linuxbrew/bin $PATH
 set fish_greeting
