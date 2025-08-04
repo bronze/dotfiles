4bc6a6e66e0c2d0907a5031302729655dcccace9
Carlos Bronze <170220+bronze@users.noreply.github.com>
2025-08-04 13:42:36 -0300

    zoxide fzf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,7 +4,8 @@ set -gx PATH /home/linuxbrew/.linuxbrew/bin $PATH
 set fish_greeting
 
 # Set up zoxide https://github.com/ajeetdsouza/zoxide
-zoxide init fish | source
+# zoxide init fish | source
+zoxide init fish --cmd cd | source # zoxide replacing the cd command
 
 # Set up fzf key bindings https://github.com/junegunn/fzf
 fzf --fish | source
